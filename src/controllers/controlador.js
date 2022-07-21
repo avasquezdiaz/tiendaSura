00df1ef8a1aa8b34b743e0057071dc704173b6a6
407 <407@academicos.cesde.edu.co>
2022-07-21 17:40:28 -0500

    se creo producto

---
+++ src/controllers/controlador.js
@@ -0,0 +1,14 @@
+//creando variables 
+
+let nombreUsuario="Andrea Vasquez"
+const EDAD_USUARIO=31
+let estaturaUsuario=1.60
+let hinchaDelVerde=false
+
+console.log(nombreUsuario)
+
+EDAD_USUARIO=22
+
+console.log(EDAD_USUARIO)
+
+
