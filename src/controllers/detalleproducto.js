f6ea3efa124417af55e2202b90399bff5fec8e58
407 <407@academicos.cesde.edu.co>
2022-08-16 17:38:51 -0500

    se crea carrito de compras

--- src/controllers/detalleproducto.js
+++ src/controllers/detalleproducto.js
@@ -4,6 +4,19 @@ console.log("estoy detalLando la compra")
 let producto=JSON.parse (localStorage.getItem("producto"))
 console.log (producto)
 
+let carrito
+if(JSON.parse(localStorage.getItem("carrito"))!=null){
+    carrito=JSON.parse(localStorage.getItem ("carrito"))
+    let pildora=document.getElementById("pildora")
+    pildora.textContent=carrito.length
+}
+
+else{
+    carrito = []
+}
+
+
+
 //referenciar la imagen del producto
 let foto=document.getElementById("imagenInfo")
 foto.src=producto.foto
@@ -16,3 +29,36 @@ nombre.textContent=producto.nombre
 
 let descripcion=document.getElementById("descripcionInfo")
 descripcion.textContent=producto.descripcion
+
+let pildora=document.getElementById("pildora")
+console.log (pildora.textContent)
+
+//escucho clic en el boton añadir carrito
+    let botonCarrito=document.getElementById("botonAgregarCarrito")
+    botonCarrito.addEventListener("click",function(evento){
+    let cantidad=document.getElementById("cantidadProducto")
+    cantidad=cantidad.value 
+    console.log("agregaste:" +cantidad)   
+
+
+//agregamos la cantidad al objeto producto
+    producto.cantidad=cantidad
+    console.log(producto)
+
+
+//agregramos el producto al carrito de compras
+
+
+    carrito.push(producto)
+    console.log(carrito)
+
+
+//agregando el carrito a la memoria
+    localStorage.setItem("carrito",JSON.stringify(carrito))
+    let cantidadCarrito=carrito.length
+    pildora.textContent=cantidadCarrito
+
+
+
+
+})
